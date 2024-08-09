c2189ad1ba219968d4fc14d7ba8350af12e3b597
BarbaraTito <barbaratito.tito@gmail.com>
2024-08-09 08:23:11 -0300

    criação de agencia e alteração

--- Conta-banco/src/BancoTerminal.java
+++ Conta-banco/src/BancoTerminal.java
@@ -1,5 +1,19 @@
-public class BancoTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class BancoTerminal{
+
+
+    public static void main (String[] args){
+ 
+       Agencia numeroAgencia = new Agencia(1021);
+        Conta numeroConta = new Conta(067-8);
+
+
+
+         Nome = "MARIO ANDRADE";
+         int numeroConta = 1021;
+         String  conta = new Conta(067-8);
+         double = 273.48
     }
+
+
 }
+
